20460f829ff65e7883f428490dc01148692772c3
soma <mrsumomo7@gmail.com>
2024-05-08 10:56:37 +0900

    Initial commit

---
+++ src/main/java/jp/houlab/mochidsuki/tad_character_engineer/TAD_Character_Engineer.java
@@ -0,0 +1,17 @@
+package jp.houlab.mochidsuki.tad_character_engineer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TAD_Character_Engineer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
